afc62b505a945fba424257d93765fbd0dffecae0
Monika Raab <monika.raab@microsoft.wsei.edu.pl>
2019-06-02 19:52:12 +0200

    Update Gra.cs

--- GraZaDuzoZaMalo/ModelGry/Gra.cs
+++ GraZaDuzoZaMalo/ModelGry/Gra.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ModelGry
 {
-    public class Gra
+    public partial class Gra
     {
         // inner types
         public enum Odpowiedz { ZaMalo = -1, Trafiono = 0, ZaDuzo = 1 }
@@ -35,20 +36,24 @@ namespace ModelGry
             wylosowana = Losuj(ZakresOd, ZakresDo);
             //LicznikRuchow = 0;
             StanGry = State.Trwa;
+            historia = new List<Ruch>();
         }
 
         public Odpowiedz Ocena( int propozycja )
         {
             LicznikRuchow++;
+            Odpowiedz odp;
             if (propozycja < wylosowana)
-                return Odpowiedz.ZaMalo;
+                odp = Odpowiedz.ZaMalo;
             else if (propozycja > wylosowana)
-                return Odpowiedz.ZaDuzo;
+                odp = Odpowiedz.ZaDuzo;
             else
             {
                 StanGry = State.Odgadnieta;
-                return Odpowiedz.Trafiono;
+                odp = Odpowiedz.Trafiono;
             }
+            historia.Add(new Ruch(propozycja, odp));
+            return odp;
         }
 
         public void Poddaj()
