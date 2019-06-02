bb5b6151b4ccc62427448ab3f806e1604f34629a
Monika Raab <monika.raab@microsoft.wsei.edu.pl>
2019-06-02 19:47:05 +0200

    Create Form1.cs

---
+++ GraZaDuzoZaMalo/GraGUI/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ModelGry;
+
+namespace GraGUI
+{
+    public partial class Form1 : Form
+    {
+        private Gra g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNowaGra_Click(object sender, EventArgs e)
+        {
+            groupBoxLosuj.Visible = true;
+            buttonNowaGra.Enabled = false;
+        }
+
+        private void buttonLosuj_Click(object sender, EventArgs e)
+        {
+            // wczytaj zakres do losowania
+            int a = int.Parse( textBoxOd.Text );
+            int b = int.Parse( textBoxDo.Text );
+
+            // utwórz grę
+            g = new Gra(a, b);
+
+            textBoxOd.Enabled = false;
+            textBoxDo.Enabled = false;
+            buttonLosuj.Enabled = false;
+
+            // wyświetl kolejne elementy formularza
+
+        }
+    }
+}
