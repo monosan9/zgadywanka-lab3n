413ca63e802121973114663028be0841897c017c
monosan9 <48621342+monosan9@users.noreply.github.com>
2019-06-16 14:13:26 +0200

    Figures

---
+++ Figures/FiguresLib/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace FiguresLib
+{
+    abstract public class Figure
+    {
+        public string Label {get; set;}
+        public Color Color { get; set; }
+        private static int counter = 0;
+
+        public Figure()
+        {
+            counter++;
+            this.Color = Color.Black;
+            Label = $"{GetType().Name} #{counter}";
+        }
+
+        abstract public void Draw();
+        public override string ToString() => $"Figure: {Label}";
+        
+    }
+}
---
+++ Figures/FiguresLib/IMeasurable1B.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FiguresLib
+{
+    public interface IMeasurable1B
+    {
+        double Lenght { get; }
+    }
+}
